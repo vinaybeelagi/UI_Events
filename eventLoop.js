a309a0160eae8251c40c82f251d762c8ebf7de40
vinaybeelagi <vinaykumar.b@neoito.com>
2024-01-12 17:56:28 +0530

    event loop example

--- eventLoop.js
+++ eventLoop.js
@@ -13,3 +13,16 @@ setTimeout(() => console.log(6));
 console.log(7);
 
 //The console output is: 1 7 3 5 2 6 4.
+
+
+// Example of a macrotask: setTimeout
+setTimeout(() => {
+    console.log("Timeout macrotask executed after 2000 milliseconds");
+  }, 2000);
+  
+  // Example of a microtask: Promise
+  Promise.resolve()
+    .then(() => console.log("Promise microtask 1"))
+    .then(() => console.log("Promise microtask 2"));
+  
+  console.log("End of the script");
